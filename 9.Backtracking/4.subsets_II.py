d18be3dbe350eb06d2d1beb865ad6da12ed91c4a
snehsuresh <snehsuresh02@gmail.com>
2024-09-29 22:03:45 -0400

    new prob

--- 9.Backtracking/4.subsets_II.py
+++ 9.Backtracking/4.subsets_II.py
@@ -1,10 +1,15 @@
 """
 Subsets II
-Solved 
+
+REFER PNG FOR [1,2,2,3]
+
+
 You are given an array nums of integers, which may contain duplicates. 
 Return all possible subsets.
 
 The solution must not contain duplicate subsets. You may return the solution in any order.
+
+
 
 Example 1:
 
@@ -17,7 +22,7 @@ Input: nums = [7,7]
 
 Output: [[],[7], [7,7]]
 
-REFER PNG FOR [1,2,2,3]
+
 """
 
 from typing import List
@@ -26,23 +31,25 @@ from typing import List
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
 
-        res = []
         nums.sort()
 
-        def backtrack(i, subset):
+        res = []
+        subset = []
+
+        def dfs(i):
             if i == len(nums):
                 res.append(subset.copy())
                 return
 
             subset.append(nums[i])
-            backtrack(i + 1, subset)
+            dfs(i + 1)
             subset.pop()
 
             while i + 1 < len(nums) and nums[i] == nums[i + 1]:
                 i += 1
-            backtrack(i + 1, subset)
+            dfs(i + 1)
 
-        backtrack(0, [])
+        dfs(0)
         return res
 
 
